94f69a9555e691d7b42c20724ebd37f9705a0e52
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-10-06 00:00:00 +0000

    Cleaned code.

--- Module.php
+++ Module.php
@@ -2,7 +2,7 @@
 
 namespace SearchHistory;
 
-if (!class_exists(\Common\TraitModule::class)) {
+if (!class_exists('Common\TraitModule', false)) {
     require_once dirname(__DIR__) . '/Common/TraitModule.php';
 }
 
